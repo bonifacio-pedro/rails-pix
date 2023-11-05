7ba157b7c6e88b885a6c08840d2a6e33eb70ecf3
bonifacio-pedro <pedrorosa.rb@gmail.com>
2023-11-04 22:00:03 -0300

    Added initial tests with RSpec

---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal = true
+
+require 'rails_helper'
+
+describe 'Trasaction' do
+  it 'payer key doesnt exists' do
+  end
+  it 'receiver key doesnt exists' do
+  end
+  it 'transaction price isnt valid' do
+  end
+end
