31c07949a2f13bd0fc9c0b6eadd68cb49c0ac315
bonifacio-pedro <pedrorosa.rb@gmail.com>
2023-11-09 22:11:28 -0300

    Refactor transaction spec

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -6,21 +6,21 @@ describe 'Trasaction' do
   it 'payer key doesnt exists' do
     payer = UserPix.new(username: 'Test', keytype: 'EMAIL', key: 'email@test.com')
     receiver = UserPix.new(username: 'Testx', keytype: 'EMAIL', key: 'email@testx.com')
-    transaction = Transaction.new(payer: "#{payer.id.to_s}x", receiver: receiver.id, price: 20.00)
+    transaction = Transaction.new(payer: "#{payer.id}x", receiver: receiver.id, price: 20.00)
     transaction.valid?
     expect(transaction.errors[:name]).to include('Invalid payer key')
   end
   it 'receiver key doesnt exists' do
     payer = UserPix.new(username: 'Test', keytype: 'EMAIL', key: 'email@test.com')
     receiver = UserPix.new(username: 'Testx', keytype: 'EMAIL', key: 'email@testx.com')
-    transaction = Transaction.new(payer: payer.id.to_s, receiver: "#{receiver.id.to_s}x", price: 20.00)
+    transaction = Transaction.new(payer: payer.id, receiver: "#{receiver.id}x", price: 20.00)
     transaction.valid?
     expect(transaction.errors[:name]).to include('Invalid receiver key')
   end
   it 'payer and receiver key doesnt exists' do
     payer = UserPix.new(username: 'Test', keytype: 'EMAIL', key: 'email@test.com')
     receiver = UserPix.new(username: 'Testx', keytype: 'EMAIL', key: 'email@testx.com')
-    transaction = Transaction.new(payer: "#{payer.id.to_s}x", receiver: "#{receiver.id.to_s}x", price: 20.00)
+    transaction = Transaction.new(payer: "#{payer.id}x", receiver: "#{receiver.id}x", price: 20.00)
     transaction.valid?
     expect(transaction.errors[:name]).to include('Invalid payer key', 'Invalid receiver key')
   end
