fb3d2dd2000e027e33d021cbec5f37133a27a86e
bonifacio-pedro <pedrorosa.rb@gmail.com>
2023-11-11 13:22:21 -0300

    Added transactions routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :transaction, only: [:index, :create, :destroy]
   resources :pix, only: [:index, :create, :destroy]
 
   root 'pix#index'
