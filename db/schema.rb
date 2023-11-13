e2e6cb6d7bbde1ab6e8fc72b354fdb10b0a93558
bonifacio-pedro <pedrorosa.rb@gmail.com>
2023-11-13 10:16:05 -0300

    Final DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_07_120439) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_12_125832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_07_120439) do
   create_table "transactions", force: :cascade do |t|
     t.string "payer", null: false
     t.string "receiver", null: false
-    t.string "price", null: false
+    t.decimal "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
